f65d719558ddd40d1617ce5af9bc28613f3bb481
blessyluis <blessyluis@gmail.com>
2025-04-03 18:32:36 -0400

    Task 7 and 8

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,6 +14,15 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header, timeout=15)
     formatted_response = json.loads(response.text)
+    if response.status_code == 400:
+        return{
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+        }
     anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
     disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
     fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
--- server.py
+++ server.py
@@ -18,10 +18,13 @@ def emotion_analyzer():
     '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        return 'Invalid text! Please try again!'
     return f"For the given statement, the system response is " \
     f"'anger': {response['anger']}, 'disgust': {response['disgust']}, " \
     f"'fear': {response['fear']}, 'joy': {response['joy']} and " \
     f"'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
